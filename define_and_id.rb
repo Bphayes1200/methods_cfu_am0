cea54c16dd4bce556b479fb553ea15c24e6470c1
Brian Hayes <71752551+Bphayes1200@users.noreply.github.com>
2022-08-31 09:09:05 -0600

    Update define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,23 @@
 
 
 "Hello World".include?("Hello")
-
+# The include? method is being called on the string object "Hello World". The argument "Hello" is passed through the include? method. 
+# The return value is true
 
 "Hello World".end_with?("Hello")
-
+# The end_with? method is being called on the string object "Hello World". The arguement "Hello" is being passed through the end_with? method. 
+# The return value is false
 
 "Hello World".end_with?("rld")
-
+# the method end_with is being called on the string object "Hello World". The arguement "rld" is being passed through the end_with? method.
+# the return value is true
 
 12.even?
-
+# The even? method is not being called upon an object. There are no arguements.
+# The return value will be a syntax error
 
 18.next
+# The next method is not being called on an object. There are no arguements. 
+# The return value will be a syntax error.
 
 
